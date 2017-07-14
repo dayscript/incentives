8ae8aa9fb8f6be965fc94411b84153a0588220ef
Juan Carlos Orrego <jcorrego@gmail.com>
2017-07-13 23:26:00 -0500

    Get goal values by date format

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Incentives\Core\Client;
+use App\Incentives\Rules\Goal;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
