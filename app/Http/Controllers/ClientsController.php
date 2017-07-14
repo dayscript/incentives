b5733b33ad85d8dbf110822f30e6c586feacf748
Juan Carlos Orrego <jcorrego@gmail.com>
2017-07-13 23:19:33 -0500

    Get goal values by date format

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -9,136 +9,136 @@ use Illuminate\Support\Facades\Storage;
 
 class ClientsController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    $clients = Client::orderBy('name')->paginate(10);
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $clients = Client::orderBy('name')->paginate(10);
 
-    return view('clients.index', compact('clients'));
-  }
-
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function apilist()
-  {
-    $results            = [];
-    $clients            = Client::orderBy('name')->get();
-    $results['clients'] = $clients;
-
-    return $results;
-  }
-
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
-    return view('clients.create');
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    sleep(1);
-    $this->validate(request(), [
-      'name' => 'required|min:5',
-    ]);
-
-    $client = Client::create(request()->all());
-    if (str_contains($client->image, 'clients/0/')) {
-      $old_file = str_replace('/storage/', '', $client->image);
-      //            dd(Storage::disk('public')->size($old_file));
-      $new_file = str_replace('clients/0/', 'clients/' . $client->id . '/', $old_file);
-      Storage::disk('public')->move($old_file, $new_file);
-      $client->image = '/storage/' . $new_file;
-      $client->save();
+        return view('clients.index', compact('clients'));
     }
-    $results            = [];
-    $results['client']  = $client;
-    $results['status']  = 'success';
-    $results['message'] = __('Cliente creado');
 
-    return $results;
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function apilist()
+    {
+        $results            = [];
+        $clients            = Client::orderBy('name')->get();
+        $results['clients'] = $clients;
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\Incentives\Core\Client $client
-   * @return \Illuminate\Http\Response
-   */
-  public function show(Client $client)
-  {
-    $results           = [];
-    $results['client'] = $client;
-    $results['status'] = 'success';
+        return $results;
+    }
 
-    return $results;
-  }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('clients.create');
+    }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  \App\Incentives\Core\Client $client
-   * @return \Illuminate\Http\Response
-   */
-  public function edit(Client $client)
-  {
-    return view('clients.edit', compact('client'));
-  }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        sleep(1);
+        $this->validate(request(), [
+          'name' => 'required|min:5',
+        ]);
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \App\Incentives\Core\Client $client
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Client $client)
-  {
-    sleep(1);
-    $this->validate(request(), [
-      'name' => 'required|min:5',
-    ]);
+        $client = Client::create(request()->all());
+        if (str_contains($client->image, 'clients/0/')) {
+            $old_file = str_replace('/storage/', '', $client->image);
+            //            dd(Storage::disk('public')->size($old_file));
+            $new_file = str_replace('clients/0/', 'clients/' . $client->id . '/', $old_file);
+            Storage::disk('public')->move($old_file, $new_file);
+            $client->image = '/storage/' . $new_file;
+            $client->save();
+        }
+        $results            = [];
+        $results['client']  = $client;
+        $results['status']  = 'success';
+        $results['message'] = __('Cliente creado');
 
-    $client->update(request()->all());
+        return $results;
+    }
 
-    $results            = [];
-    $results['client']  = $client;
-    $results['status']  = 'success';
-    $results['message'] = __('Datos actualizados');
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Incentives\Core\Client $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
+    {
+        $results           = [];
+        $results['client'] = $client;
+        $results['status'] = 'success';
 
-    return $results;
-  }
+        return $results;
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  \App\Incentives\Core\Client $client
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy(Client $client)
-  {
-    $results = [];
-    $client->delete();
-    $results['status']  = 'success';
-    $results['message'] = __('Cliente eliminado');
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Incentives\Core\Client $client
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Client $client)
+    {
+        return view('clients.edit', compact('client'));
+    }
 
-    return $results;
-  }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Incentives\Core\Client $client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Client $client)
+    {
+        sleep(1);
+        $this->validate(request(), [
+          'name' => 'required|min:5',
+        ]);
+
+        $client->update(request()->all());
+
+        $results            = [];
+        $results['client']  = $client;
+        $results['status']  = 'success';
+        $results['message'] = __('Datos actualizados');
+
+        return $results;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Incentives\Core\Client $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        $results = [];
+        $client->delete();
+        $results['status']  = 'success';
+        $results['message'] = __('Cliente eliminado');
+
+        return $results;
+    }
 
     /**
      * List date goal values
@@ -148,12 +148,40 @@ class ClientsController extends Controller
      */
     public function dategoalvalues(Client $client, $date = null)
     {
-        if(!$date)$date = Carbon::now()->toDateString();
-        $results = [];
-        $results['goal_values'] = collect([]);
-        foreach ($client->goals as $goal){
-            $results['goal_values'] = $results['goal_values']->merge($goal->entities()->wherePivot('date',$date)->get());
+        if (!$date) $date = Carbon::now()->toDateString();
+        $results                = [];
+        $results['goal_values'] = [];
+        foreach ($client->goals as $goal) {
+            foreach ($goal->entities()->wherePivot('date', $date)->get() as $entity) {
+                $percentage = round(100 * $entity->pivot->real / $entity->pivot->value, 2);
+                if ($goal->modifier == 'modifier1') {
+                    $mod_percentage = Goal::modifier1($percentage);
+                } else if ($goal->modifier == 'modifier2') {
+                    $mod_percentage = Goal::modifier2($percentage);
+                } else if ($goal->modifier == 'modifier3') {
+                    $mod_percentage = Goal::modifier3($percentage);
+                } else if ($goal->modifier == 'modifier4') {
+                    $mod_percentage = Goal::modifier4($percentage);
+                } else {
+                    $mod_percentage = $percentage;
+                }
+                $percentage_weighed       = $mod_percentage * ($goal->weight / 100);
+                $results['goal_values'][] = [
+                  'id'                  => $entity->pivot->id,
+                  'identification'      => $entity->identification,
+                  'name'                => $entity->name,
+                  'goal_id'             => $entity->pivot->goal_id,
+                  'value'               => $entity->pivot->value,
+                  'real'                => $entity->pivot->real,
+                  'percentage'          => $percentage,
+                  'percentage_modified' => $mod_percentage,
+                  'percentage_weighed'  => $percentage_weighed,
+                  'date'                => $entity->pivot->date,
+                  'created_at'          => $entity->pivot->created_at,
+                ];
+            }
         }
+
         return $results;
 
     }
--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,13 @@ Route::get('/test', function () {
     \Illuminate\Support\Facades\Log::info('OK');
     return 'API authentication OK';
 });
+Route::get('clients/{client}/dategoalvalues/{date?}', 'ClientsController@dategoalvalues');
 Route::get('clients', 'ClientsController@apilist');
 Route::get('entities/{identification}', 'EntitiesController@showByIdentification');
 Route::post('entities/{identification}/addvalue', 'EntitiesController@addvalue');
 Route::post('entities/{identification}/addgoalvalue', 'EntitiesController@addgoalvalue');
 Route::get('entities/{identification}/delvalue/{id}', 'EntitiesController@delvalue');
 Route::get('entities/{identification}/delgoalvalue/{id}', 'EntitiesController@delgoalvalue');
-Route::get('clients/{client}/dategoalvalues/{date?}', 'ClientsController@dategoalvalues');
+
 
 
