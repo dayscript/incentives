81bb12f5cbf07213a8ef0dd23239eb2567476399
Ivan Del Valle <idelvalle@grupo-link.com>
2018-07-03 09:26:08 -0500

    ajuste de router api y web para version 2V

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,71 @@ Route::get('/test', function () {
     \Illuminate\Support\Facades\Log::info('OK');
     return 'API authentication OK';
 });
+
 Route::get('clients/{client}/dategoalvalues/{date?}', 'ClientsController@dategoalvalues');
 Route::get('clients', 'ClientsController@apilist');
-Route::get('entities/{identification}', 'EntitiesController@showByIdentification');
-Route::post('entities/{identification}/addvalue', 'EntitiesController@addvalue');
-Route::post('entities/{identification}/addgoalvalue', 'EntitiesController@addgoalvalue');
-Route::get('entities/{identification}/delvalue/{id}', 'EntitiesController@delvalue');
-Route::get('entities/{identification}/delgoalvalue/{id}', 'EntitiesController@delgoalvalue');
+Route::get('types', 'TypeController@apilist');
+Route::get('indicators', 'IndicatorController@apilist');
+Route::get('roles', 'RolesController@apilist');
+
+Route::group(['prefix' => 'entities/'], function () {
+
+	Route::post('addentities', 'EntitiesController@addentities');
+	Route::get('addentities', 'EntitiesController@addentities');
+
+	Route::post('addentities/s', 'EntitiesController@addentitiesSOAP');
+	Route::get('addentities/s', 'EntitiesController@addentitiesSOAP');
+
+	Route::post('editentities', 'EntitiesController@editentities');
+	Route::get('editentities', 'EntitiesController@editentities');
+
+	Route::post('editentities/s', 'EntitiesController@editentitiesSOAP');
+	Route::get('editentities/s', 'EntitiesController@editentitiesSOAP');
+
+	Route::get('formonth/{date}/{rol}', 'EntitiesController@showByMonth');
+	
+
+	Route::get('{identification}', 'EntitiesController@showByIdentification');
+
+	Route::get('{identification}/formonth/{date}', 'EntitiesController@showByIdentificationtoMonth');
+
+	Route::post('{identification}/addvalue', 'EntitiesController@addvalue');
+	Route::post('{identification}/addgoalvalue', 'EntitiesController@addgoalvalue');
+	Route::post('{identification}/addvaluetype', 'EntitiesController@addvaluetype');
+	Route::get('{identification}/delvalue/{id}', 'EntitiesController@delvalue');
+	Route::get('{identification}/delgoalvalue/{id}', 'EntitiesController@delgoalvalue');
+	Route::get('{identification}/yearentity', 'EntitiesController@showIndicators');
+
+	Route::get('{identification}/html', 'EntitiesController@showHtml');
+
+});	
+//API user routes
+Route::group(['prefix' => 'leads/'], function () {
+	Route::get('addleads', 'LeadController@addleads');
+	Route::post('addleads', 'LeadController@addleads');
+	Route::get('addleads/s', 'LeadController@addleadsSOAP');
+	Route::post('addleads/s', 'LeadController@addleadsSOAP');
+	Route::post('stages', 'LeadController@addstages');
+	Route::get('stages/s', 'LeadController@addstagesSOAP');
+	Route::post('stages/s', 'LeadController@addstagesSOAP');
+
+	Route::get('test/s', 'LeadController@testSOAP');
+	Route::post('test/s', 'LeadController@testSOAP');
+
+});
+
+Route::group(['middleware' => ['cors'], 'prefix' => 'v1/'], function(){
+	//Route::middleware('auth:api')->get('/test', function () {
+	Route::get('test', function () {
+	    \Illuminate\Support\Facades\Log::info('OK');
+	    return 'API authentication OK';
+	});
+    Route::post('entities/{identification}/addgoalvalue', 'EntitiesController@addgoalvalue');
+    Route::post('entities/{identification}/addvaluetype', 'EntitiesController@addvaluetype');
+    Route::get('entities/{identification}/addgoalvalue', 'EntitiesController@addgoalvalue');
+    Route::get('entities/{identification}/formonth/{date}', 'EntitiesController@showByIdentificationtoMonth');
+    Route::get('entities/{identification}/html', 'EntitiesController@showHtml');
+});
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,25 @@ Route::get('docs/{folder?}/{option?}', 'DocsController@index');
 Route::get('/', 'HomeController@index');
 
 Route::get('users/api','UsersController@apitokens');
-Route::resource('users','UsersController');
+
+
+
 Route::post('users/{user}','UsersController@update');
-Route::resource('uploads','Utils\UploadsController');
-Route::resource('clients','ClientsController');
+
+Route::get('toquenworpres','EntitiesController@createUserWordpres');
+Route::get('prueba','EntitiesController@purebaUserWorprwss');
+
+
 //Route::get('uploads/{folder}/{file}','Utils\UploadsController@show');
 
-
+Route::resource('uploads','Utils\UploadsController');
+Route::resource('clients','ClientsController');
+Route::resource('modifier','ModifierController');
+Route::resource('users','UsersController');
+Route::resource('roles','RolesController');
+Route::resource('indicator','IndicatorController');
 Route::resource('rules','RulesController');
 Route::resource('goals','GoalsController');
 Route::resource('entities','EntitiesController');
+
+Route::get('entities/formonth/{date}/{rol}', 'EntitiesController@showByMonthHtml');
