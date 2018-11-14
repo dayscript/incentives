b44db335af1d1434a1f8d03e250e497d2be30d0d
aacevedoday <you@example.com>
2018-11-14 11:38:04 -0500

    update

--- app/Http/Controllers/RedemptionController.php
+++ app/Http/Controllers/RedemptionController.php
@@ -61,7 +61,7 @@ class RedemptionController extends Controller
 
 
       $results['status']  = 200;
-      $results['message'] = __('Has redimido con exito ' . $redemption->value . " kokoripesos\n, se ha enviado una notificación a tu correo electronico con las instrucciones para disfrutarlo" );
+      $results['message'] = __('Has redimido con éxito ' . $redemption->value . " kokoripesos\n, se ha enviado una notificación a tu correo electrónico con las instrucciones para disfrutarlo" );
 
       return $results;
     }
