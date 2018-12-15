91b942f907e8ec6ae582c18cb14cdb93edf1132b
aacevedoday <aacevedo@dayscript.com>
2018-12-15 12:46:07 -0500

    update

--- app/Console/Commands/UpdateEntities.php
+++ app/Console/Commands/UpdateEntities.php
@@ -65,7 +65,7 @@ class UpdateEntities extends Command
 
         $dbConn = new Mysql($pdoConn);
 
-        $cedulas = $dbConn->fetchRowMany('SELECT cedula FROM ju1 limit 0,5000');
+        $cedulas = $dbConn->fetchRowMany('SELECT cedula FROM ju1 limit 5000,10000');
 
         foreach( $cedulas as $cedula ){
 
