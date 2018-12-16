b4a697d991d59813d51cfbccd156e09fdb4dbbbe
aacevedoday <you@example.com>
2018-12-16 10:41:12 -0500

    update

--- app/Console/Commands/UpdateEntities.php
+++ app/Console/Commands/UpdateEntities.php
@@ -65,13 +65,14 @@ class UpdateEntities extends Command
 
         $dbConn = new Mysql($pdoConn);
 
-        $cedulas = $dbConn->fetchRowMany('SELECT cedula FROM ju1 limit 10000,15000');
-
+        $cedulas = $dbConn->fetchRowMany('SELECT cedula FROM ju1 limit 18773,2000');
+        $cont = 0;
         foreach( $cedulas as $cedula ){
+          $cont ++;
 
           $cedula = $cedula['cedula'];
 
-          $this->question('Buscando: '. $cedula);
+          $this->question($cont.' - Buscando: '. $cedula);
           $entity = Entity::where('identification','=',$cedula)->get()->first();
 
           if(!$entity){
