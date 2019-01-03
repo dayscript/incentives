0cbc6e3373be18446014edd0c96eb4af4e767de0
aacevedoday <you@example.com>
2019-01-03 10:23:12 -0500

    update

--- app/Incentives/Core/Entity.php
+++ app/Incentives/Core/Entity.php
@@ -307,9 +307,9 @@ class Entity extends Model
         $invoice_total += array_sum($invoice_points);
       }
 
-      print_r(array_sum($invoice_total));
+      print_r($invoice_total);
       print_r(array_sum($entity_goals));
-      print_r(array_sum($rule_points));
+      print_r($rule_points);
       print_r(array_sum($redemptions));
 
       // $total = ( array_sum($invoices) + ( array_sum($entity_goals) + $rule_points ) ) - array_sum($redemptions);
