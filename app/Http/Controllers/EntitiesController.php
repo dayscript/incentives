fcac3619caf219fd3e2b6340f1e04985ff7aa3de
aacevedoday <you@example.com>
2019-02-11 08:52:27 -0500

    update

--- app/Http/Controllers/EntitiesController.php
+++ app/Http/Controllers/EntitiesController.php
@@ -514,7 +514,7 @@ class EntitiesController extends Controller
     }
 
     public function devel(){
-      $entitys = Entity::where('type_id',1,'=')->whereNull('zoho_id')->first();
+      $entitys = Entity::where('type_id',1,'=')->where('zoho_lead_to_contact',1,'=')->whereNull('zoho_id')->first();
       $entitys->entityInformation;
       return $entitys;
 
