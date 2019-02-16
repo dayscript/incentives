7cccd52536ec5eea82986bf6ad930f0f5d05191d
aacevedoday <you@example.com>
2019-02-16 18:59:42 -0500

    update

--- app/Http/Controllers/EntitiesController.php
+++ app/Http/Controllers/EntitiesController.php
@@ -629,7 +629,7 @@ class EntitiesController extends Controller
 
       foreach ($ITEMS as $key => $item) {
 
-        $entity = Entity::where('identification','=',$item[0]);
+        $entity = Entity::where('identification','=',$item[0])->get();
 
         dd($entity);
         $entity->zoho_id = $item[1];
